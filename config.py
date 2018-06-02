c3f28130c613c208e1021c78e8f07f4c4e3f1393
gmark <gmark4212@gmail.com>
2018-06-02 14:27:57 +0700

    Добавил ключи hitbtc в конфиг

--- config.py
+++ config.py
@@ -9,3 +9,7 @@ APP_VERSION = '0.1'
 
 # dirs
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# keys
+HITBTC_API = 'aeac732a33236d840e4beccfced63754'
+HITBTC_SECRET = '17e70f8a123e2b90fd0440c0deb6f3a6'
