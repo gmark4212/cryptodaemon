6d46bd7da8340cadf0703d493884b7d3dc3f1dad
gmark <gmark4212@gmail.com>
2018-06-04 04:51:21 +0700

    Написал подбор оптимальных для покупки монет в Strategy

--- config.py
+++ config.py
@@ -2,13 +2,27 @@
 # -*- coding: utf-8 -*-
 
 import os
+from ccxt import hitbtc2
 
 # general
 APP_NAME = 'CryptoDaemonBot'
-APP_VERSION = '0.1'
+APP_VERSION = '0.2'
 
 # dirs
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # keys
 API_KEYS = {'hitbtc': dict(api='aeac732a33236d840e4beccfced63754', secret='17e70f8a123e2b90fd0440c0deb6f3a6')}
+
+# exchanges
+DEFAULT_EXCHANGE = hitbtc2({
+    "apiKey": API_KEYS['hitbtc']['api'],
+    "secret": API_KEYS['hitbtc']['secret'],
+    "enableRateLimit": True,
+})
+
+# currency
+BASE_TICKER = 'BTC'
+USD = 'USD'
+
+
--- strategy.py
+++ strategy.py
@@ -1,13 +1,46 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from config import *
+from coinmarketcap import Market
 
 
 class Strategy:
-    def __init__(self):
+    def __init__(self, exchange=None):
+        self.exchange = exchange if exchange else DEFAULT_EXCHANGE
         self.type = 'scalping'
         self.purchase_different_coins = 3
-        self.drop_range_to_buy_pct = [10, 40]
+        self.drop_range_to_buy_pct = range(-40, -10)
         self.deposit_threshold_pct = 50
-        self.capitalization_threshold_usd = 20000000000
+        self.capitalization_threshold_usd = float(50000000.0)
         self.market_volume_threshold_usd_24h = 500000
         self.your_margin_pct = 10
+        self.market = Market()
+        self.drops = None
+        self.suitable_coins_marketcap = {}
+        self.coins_listing = {item['symbol']: {'id': item['id'], 'name': item['name']} for item in self.market.listings()['data']}
+
+    def fetch_suitable_coins(self):
+        tickers = self.exchange.fetch_tickers()
+        self.drops = {pair: data for pair, data in tickers.items()
+                      if pair.endswith(BASE_TICKER) and int(data['percentage']) in self.drop_range_to_buy_pct}
+
+        if self.market:
+            for pair, market_data in self.drops.items():
+                ticker = pair.split('/')[0]
+                if ticker in self.coins_listing:
+                    market_cap_id = self.coins_listing[ticker]['id']
+                    if bool(market_cap_id):
+                        market_data = self.market.ticker(market_cap_id, convert=USD)['data']['quotes'][USD]
+                        capital = market_data['market_cap']
+                        if isinstance(capital, float):
+                            if capital >= self.capitalization_threshold_usd:
+                                    print(ticker, capital)
+                                    self.suitable_coins_marketcap[ticker] = market_data
+                else:
+                    print('Не могу узнать капитализацию: {}, пропускаю'.format(ticker))
+
+
+# if __name__ == '__main__':
+#
+#     s = Strategy()
+#     s.fetch_suitable_coins()
