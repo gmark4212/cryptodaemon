3c0dbf4415768a8d3e997dfd944b01c25139570b
gmark <gmark4212@gmail.com>
2018-06-04 21:59:08 +0700

    добавил метод криптобота для выставления ордеров sell-buy

--- cryptobot.py
+++ cryptobot.py
@@ -15,8 +15,7 @@ class CryptoBot:
 
     def fetch_balance(self, base_currency_only=True):
         if self.strategy:
-            ex = self.strategy.exchange
-            data = ex.fetch_balance()
+            data = self.strategy.exchange.fetch_balance()
             currencies_balances = data['info']
             if base_currency_only:
                 for balance in currencies_balances:
@@ -27,8 +26,12 @@ class CryptoBot:
 
     def get_summ_to_spend_to_buy(self):
         if self.strategy and self.base_balance>0:
-            return self.base_balance * (self.strategy.deposit_threshold_pct/100)
+            return self.base_balance * ((100-self.strategy.deposit_threshold_pct)/100)
         return 0
+
+    def place_order(self, symbol, amount, price, side=None):
+        if bool(side) and bool(symbol) and amount>0 and price>0:
+            self.strategy.exchange.create_order(symbol, 'limit', side, amount, price)
 
     def start_trading(self):
         pass
