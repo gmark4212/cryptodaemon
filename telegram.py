72f13b9f807fc4f67a06d5dd509382497d891c47
Dmedichi <dashamatisova@gmail.com>
2018-06-07 21:23:47 +0300

    Создан бот, должен приветствовать при старте Пока только функция, без класса

--- telegram.py
+++ telegram.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import telebot
+
+TOKEN = '567981171:AAE0yeFOkKUqGs00SILkFDrbP8m8wcCqIcs'
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, 'Welcome!')
 
 
-class TelegramBot:
-    pass
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+bot.polling()
+
+# class TelegramBot:
+#     pass
+
