ad6e342891bf677792ccb99ef4318649aa740743
Dmedichi <dashamatisova@gmail.com>
2018-06-09 09:09:15 +0300

    добавлены кнопки главного меню

--- telegram.py
+++ telegram.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import telebot
+from telebot import types
 
 TOKEN = '567981171:AAE0yeFOkKUqGs00SILkFDrbP8m8wcCqIcs'
 
 bot = telebot.TeleBot(TOKEN)
+
+markup = types.ReplyKeyboardMarkup(row_width=1)
+btn_start = types.KeyboardButton('запустить CryptoBot')
+btn_stop = types.KeyboardButton('остановить CryptoBot')
+btn_help = types.KeyboardButton('help')
+markup.add(btn_start, btn_stop, btn_help)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
