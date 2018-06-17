4d4b92f6fd50bc218469cbef47dbca8f0e4e62c1
gmark <gmark4212@gmail.com>
2018-06-17 20:06:28 +0700

    развитие обработчика POST запросов

--- httpserver.py
+++ httpserver.py
@@ -4,15 +4,28 @@ from config import *
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
 import json
+from strategy import Strategy
+from cryptobot import CryptoBot
+from subprocess import Popen
 
 
-class POSTRequestHandler(BaseHTTPRequestHandler):
+class PostHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
         data = json.loads(body.decode(ENCODING))
         print(data)
+        self.process_query(data)
+
+    def process_query(self, data):
+        if 'request-type' in data:
+            if data['request-type'] == 'cryptobot-start':
+                strategy_settings = data['strategy-settings']
+                print(strategy_settings)
+                strategy = Strategy(strategy_settings)
+                Popen("x-terminal-emulator -e python3 cryptobot.py -e", shell=True)
+                self.respond('OK')
 
     def respond(self, answer):
         self.send_response(200)
@@ -23,5 +36,5 @@ class POSTRequestHandler(BaseHTTPRequestHandler):
 
 
 if __name__ == '__main__':
-    httpd = HTTPServer((DEFAULT_HOST, API_PORT), POSTRequestHandler)
+    httpd = HTTPServer((DEFAULT_HOST, API_PORT), PostHandler)
     httpd.serve_forever()
