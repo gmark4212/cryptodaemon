bd149e6f4fc506c136377b25169c10f8ddc66c8e
Dmedichi <dashamatisova@gmail.com>
2018-06-17 05:46:19 +0300

    исправлены ошибки

--- telegram.py
+++ telegram.py
@@ -23,12 +23,6 @@ if __name__ == '__main__':
 
     bot = TelegramBot(TOKEN)
 
-    keyboard = types.ReplyKeyboardMarkup(row_width=1)
-    bt_run = types.KeyboardButton('Запустить CryptoBot')
-    bt_balance = types.KeyboardButton('Текущий баланс')
-    bt_stop_order = types.KeyboardButton('Остановить CryptoBot')
-    keyboard.add(bt_run, bt_balance, bt_stop_order)
-    hidden_keyboard = types.ReplyKeyboardRemove()
 
     @bot.message_handler(commands=['start'])
     def start(message):
@@ -36,11 +30,11 @@ if __name__ == '__main__':
         text = message.text
         if chatId not in bot.users:
             bot.send_message(chatId, "Hello! I am Bot. What is you name?")
-            bot.register_next_step_handler(message, process_KEY_step)
+            bot.register_next_step_handler(message, process_NAME_step)
         else:
-            bot.send_message(chatId, "Welcome, {}!".format(text), reply_markup=keyboard)
+            bot.send_message(chatId, "Welcome, {}!".format(text))
 
-    def process_KEY_step(message):
+    def process_NAME_step(message):
         chatId = message.chat.id
         name = message.text
         bot.users[chatId] = name
@@ -59,52 +53,37 @@ if __name__ == '__main__':
             bot.register_next_step_handler(message, process_SECRET_step)
 
 
-    def process_last_step(message):
-        chatId = message.chat.id
-        SECRET = message.text
-        if SECRET == 'secret':
-            bot.send_message(chatId, "Welcome, {}!".format(bot.users[chatId]))
-            command_help(message)
-        else:
-            bot.send_message(chat_id, 'SECRET KEY: ')
-            bot.register_next_step_handler(message, process_SECRET_step)
-
-
     @bot.message_handler(commands=['help'])
     def command_help(message):
         cid = message.chat.id
         help_text =  " Доступны следующие команды: \n"
         for key in COMMANDS:
-            help_text += "/" + key + ": "
-            help_text += COMMANDS[key] + "\n"
+            help_text += "/" + key + ": " + COMMANDS[key] + "\n"
             bot.send_message(cid, help_text)
 
 
     @bot.message_handler(commands=['run'])
-    @bot.message_handler(func=lambda message: True)
     def run_cryptobot(message):
-        if message.text == '/run' or message.text == 'Запустить CryptoBot':
-            bot.send_message(message.chat.id, 'CryptoBot запущен', reply_markup=keyboard)
-            bot.cryptobot.start_trading()
+        bot.cryptobot.start_trading()
+        chatId = message.chat.id
+        bot.send_message(chatId, 'CryptoBot запущен')
 
 
     @bot.message_handler(commands=['stop'])
-    @bot.message_handler(func=lambda message: True)
     def stop_cryptobot(message):
-        if message.text == '/stop' or message.text == 'Остановить CryptoBot':
-            bot.cryptobot.stop_trading()
-            bot.send_message(message.chat.id, 'CryptoBot остановлен', reply_markup=hidden_keyboard)
+        bot.cryptobot.keep_working = False
+        bot.cryptobot.stop_trading()
+        chatId = message.chat.id
+        bot.send_message(chatId, 'CryptoBot остановлен')
 
 
     @bot.message_handler(commands=['balance'])
-    @bot.message_handler(func=lambda message: True)
     def balance(message):
-        if message.text == '/balance' or message.text == 'Текущий баланс':
-            fetch_balance = bot.cryptobot.fetch_balance()
-            bot.send_message(message.chat.id, 'Ваш баланс: {}'.format(fetch_balance), reply_markup=keyboard)
+        fetch_balance = bot.cryptobot.fetch_balance()
+        bot.send_message(message.chat.id, 'Ваш баланс: {}'.format(fetch_balance))
 
 
-    bot.polling()
+    bot.polling(none_stop=True)
 
 
 
