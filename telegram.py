c41c033999035f05d1c28c3eae2672044a4564d5
Dmedichi <dashamatisova@gmail.com>
2018-06-23 05:19:32 +0300

    исправлены ошибки

--- telegram.py
+++ telegram.py
@@ -12,96 +12,96 @@ from storage import *
 from config import *
 
 
-class TelegramBot(TeleBot):
-
-    def __init__(self, TOKEN):
-        super().__init__(token=TOKEN)
-        self.token = TOKEN
-        self.users = {}
-        self.cryptobot = CryptoBot(Strategy(), emulation_mode=True)
+bot = TelegramBot(TOKEN)
+users = {}
 
 
-if __name__ == '__main__':
+@bot.message_handler(commands=['start'])
+def command_start(message):
+    chatId = message.chat.id
+    text = message.text
+    if chatId not in users:
+        # спрашивает имя и отправляет на следующий этап регистрации - секретный ключ
+        bot.send_message(chatId, "Hello! I am Bot.\nWhat is you name?")
+        bot.register_next_step_handler(message, process_NAME_step)
+        # иначе
+    else:
+        # приветствует
+       bot.send_message(chatId, f"Welcome, {text}!")
 
-    bot = TelegramBot(TOKEN)
+
+def process_NAME_step(message):
+    "запрашивает секретный ключ"
+    chatId = message.chat.id
+    name = message.text
+    # раннее полученное имя сохраняет
+    users[chatId] = name
+    bot.send_message(chatId, 'SECRET KEY: ')
+    bot.register_next_step_handler(message, process_SECRET_step)
 
 
-    @bot.message_handler(commands=['start'])
-    def start(message):
-        "проверяет, обращался ли пользователь к нему раньше"
-        chatId = message.chat.id
-        text = message.text
-        # если пользователя нет в users
-        if chatId not in bot.users:
-            # спрашивает имя и отправляет на следующий этап регистрации - секретный ключ
-            bot.send_message(chatId, "Hello! I am Bot. What is you name?")
-            bot.register_next_step_handler(message, process_NAME_step)
-            # иначе
-        else:
-            # приветствует
-            bot.send_message(chatId, f"Welcome, {text}!")
-
-    def process_NAME_step(message):
-        "запрашивает секретный ключ"
-        chatId = message.chat.id
-        name = message.text
-        # раннее полученное имя сохраняет
-        bot.users[chatId] = name
+def process_SECRET_step(message):
+    "проверяет правильно ли пользователь ввел код"
+    chatId = message.chat.id
+    SECRET = message.text
+    # если все гуд
+    if SECRET == 'secret':
+        # приветствует и предоставляет информацию от командах
+        bot.send_message(chatId, f"Welcome, {users[chatId]}!")
+        command_help(message)
+    else:
+        # в случае неправильного ввода опять запрашивает ключ
         bot.send_message(chatId, 'SECRET KEY: ')
         bot.register_next_step_handler(message, process_SECRET_step)
 
 
-    def process_SECRET_step(message):
-        "проверяет правильно ли пользователь ввел код"
-        chatId = message.chat.id
-        SECRET = message.text
-        #если все гуд
-        if SECRET == 'secret':
-            # приветствует и предоставляет информацию от командах
-            bot.send_message(chatId, f"Welcome, {bot.users[chatId]}!")
-            command_help(message)
-        else:
-            # в случае неправильного ввода опять запрашивает ключ
-            bot.send_message(chatId, 'SECRET KEY: ')
-            bot.register_next_step_handler(message, process_SECRET_step)
+@bot.message_handler(commands=['help'])
+def command_help(message):
+    chatId = message.chat.id
+    msg = text(bold('Доступны следующие команды: \n'),
+               '/help - Предоставляет информацию о доступных командах\n',
+               '/run - Запускает CryptoBot\n', '/stop - Останавливает CryptoBot\n',
+               '/balance - Предоставляет информацию о текущем балансе')
+    bot.send_message(chatId, msg, parse_mode=ParseMode.MARKDOWN)
+
+@bot.message_handler(commands=['run'])
+def run_cryptobot(message):
+    "запусткает криптобот и сообщает об этом "
+    try:
+        cryptobot = CryptoBot(Strategy(), emulation_mode=True)
+        cryptobot.start_trading()
+    except KeyError:
+        pass
+
+    bot.send_message(message.chat.id, 'CryptoBot запущен')
 
 
-    @bot.message_handler(commands=['help'])
-    def command_help(message):
-        chatId = message.chat.id
-        msg = text(bold('Доступны следующие команды: \n'),
-                   '/help - Предоставляет информацию о доступных командах\n',
-                   '/run - Запускает CryptoBot\n',
-                   '/stop - Останавливает CryptoBot\n',
-                   '/balance - Предоставляет информацию о текущем балансе')
-        bot.send_message(chatId, msg, parse_mode=ParseMode.MARKDOWN)
+@bot.message_handler(commands=['balance'])
+def balance(message):
+    "выводит информацию о текущем балансе"
+    try:
+        cryptobot = CryptoBot(Strategy(), emulation_mode=True)
+        fetch_balance = cryptobot.fetch_balance()
+    except Exception:
+        pass
+    else:
+        bot.send_message(message.chat.id, f'Ваш баланс: {str(fetch_balance)}')
 
 
-    @bot.message_handler(commands=['run'])
-    def run_cryptobot(message):
-        "запусткает криптобот и сообщает об этом "
-        bot.cryptobot.start_trading()
-        chatId = message.chat.id
-        bot.send_message(chatId, 'CryptoBot запущен')
+@bot.message_handler(commands=['stop'])
+def stop_cryptobot(message):
+    "останавлявает криптобот и сообщает об этом"
+    try:
+        cryptobot = CryptoBot(Strategy(), emulation_mode=True)
+        cryptobot.stop_trading()
+    except Exception:
+        pass
+    else:
+        bot.send_message(message.chat.id, 'CryptoBot остановлен')
 
 
-    @bot.message_handler(commands=['stop'])
-    def stop_cryptobot(message):
-        "останавлявает криптобот и сообщает об этом"
-        bot.cryptobot.keep_working = False
-        bot.cryptobot.stop_trading()
-        chatId = message.chat.id
-        bot.send_message(chatId, 'CryptoBot остановлен')
 
-
-    @bot.message_handler(commands=['balance'])
-    def balance(message):
-        "выводит информацию о текущем балансе"
-        fetch_balance = bot.cryptobot.fetch_balance()
-        bot.send_message(message.chat.id, f'Ваш баланс: {fetch_balance}')
-
-
-    bot.polling(none_stop=True)
+bot.polling(none_stop=True)
 
 
 
